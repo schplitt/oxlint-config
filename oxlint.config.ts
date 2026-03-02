3955a981cabeb08bf907afa96baadcd33d5ee869
schplitt <schplitt.contact@gmail.com>
2026-03-02 18:59:05 +0100

    chore: update autofix workflow to include build step and enhance comments in config

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,3 +1,4 @@
 import schplitt from './dist/index.mjs'
-
+// if a typescript error is reported here
+// you need to run `pnpm build` in the project root to update the dist files
 export default schplitt()
